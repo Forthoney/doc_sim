f096d50b39be059bf2d153527e2e63ac2fe5404d
Forthoney <castlehoneyjung@gmail.com>
2023-08-18 09:21:09 -0400

    add trivial tests

--- spec/locality_sensitive_hashing_spec.rb
+++ spec/locality_sensitive_hashing_spec.rb
@@ -7,3 +7,19 @@ RSpec.describe LocalitySensitiveHashing do
     expect(LocalitySensitiveHashing::VERSION).not_to be nil
   end
 end
+
+RSpec.describe Minhash::Minhash do
+  it "hashes different sets to different signatures" do
+    minhash = Minhash::Minhash.new
+    doc1 = Set["abc", "def", "ghi"]
+    doc2 = Set["jkl", "mno", "pqr"]
+    expect(minhash.signature(doc1)).not_to eq minhash.signature(doc2)
+  end
+
+  it "hashes identical sets to the same signature" do
+    minhash = Minhash::Minhash.new
+    doc1 = Set["abc", "def", "ghi"]
+    doc2 = Set["abc", "def", "ghi"]
+    expect(minhash.signature(doc1)).to eq minhash.signature(doc2)
+  end
+end
