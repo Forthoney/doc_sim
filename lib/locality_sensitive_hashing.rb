7b145061939c21f7cde1d056a06fae38997f4d12
Forthoney <castlehoneyjung@gmail.com>
2023-08-18 08:31:40 -0400

    initial commit

---
+++ lib/locality_sensitive_hashing.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "locality_sensitive_hashing/version"
+
+module LocalitySensitiveHashing
+  class Error < StandardError; end
+  # Your code goes here...
+end
