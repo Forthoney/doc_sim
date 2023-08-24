b20061ef9e6e6f388fe95d998ec5d6cca9368a78
Forthoney <castlehoneyjung@gmail.com>
2023-08-24 13:29:07 -0400

    add lsh test cases

--- spec/locality_sensitive_hashing_spec.rb
+++ spec/locality_sensitive_hashing_spec.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+# For brevity, "o" indicates match, "x" indicates mismatch
+
 RSpec.describe LocalitySensitiveHashing::LocalitySensitiveHashing do
   context "with b = 2 and r = 2" do
-    it "recognizes identical signatures as the same" do
+    it "recognizes identical signatures as similar" do
       lsh = LocalitySensitiveHashing::LocalitySensitiveHashing.new(2, 2)
       sig1 = [1, 2, 3, 4]
       sig2 = [1, 2, 3, 4]
@@ -10,6 +12,46 @@ RSpec.describe LocalitySensitiveHashing::LocalitySensitiveHashing do
       lsh.insert(sig2, 2)
 
       expect(lsh.similar_pairs).to eq Set[[1, 2]]
+    end
+    
+    it "recognizes completely different signatures as not similar" do
+      lsh = LocalitySensitiveHashing::LocalitySensitiveHashing.new(2, 2)
+      sig1 = [1, 2, 3, 4]
+      sig2 = [5, 6, 7, 8]
+      lsh.insert(sig1, 1)
+      lsh.insert(sig2, 2)
+
+      expect(lsh.similar_pairs).to eq Set.new
+    end
+
+    it "recognizes ooxx as similar" do
+      lsh = LocalitySensitiveHashing::LocalitySensitiveHashing.new(2, 2)
+      sig1 = [1, 2, 3, 4]
+      sig2 = [1, 2, 7, 8]
+      lsh.insert(sig1, 1)
+      lsh.insert(sig2, 2)
+
+      expect(lsh.similar_pairs).to eq Set[[1, 2]]
+    end
+
+    it "recognizes xxoo as similar" do
+      lsh = LocalitySensitiveHashing::LocalitySensitiveHashing.new(2, 2)
+      sig1 = [1, 2, 3, 4]
+      sig2 = [5, 6, 3, 4]
+      lsh.insert(sig1, 1)
+      lsh.insert(sig2, 2)
+
+      expect(lsh.similar_pairs).to eq Set[[1, 2]]
+    end
+
+    it "recognizes xoxo as not similar" do
+      lsh = LocalitySensitiveHashing::LocalitySensitiveHashing.new(2, 2)
+      sig1 = [1, 2, 3, 4]
+      sig2 = [5, 2, 6, 4]
+      lsh.insert(sig1, 1)
+      lsh.insert(sig2, 2)
+
+      expect(lsh.similar_pairs).to eq Set.new
     end
   end
 end
